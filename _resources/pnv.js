7a5fa4a36d283e1b4c6e334d7535d5bf8dc3dea0
jmkorhonen <jmkorhonen@gmail.com>
2021-03-28 16:34:29 +0300

    Version 2

--- _resources/pnv.js
+++
@@ -1,9 +0,0 @@
-/* Turn of animation */
-$(document).ready(function(){
- //jQuery.fx.off = true;
- $('#btnShow').click(function(){
-    $('#dvText').toggle("slow");
- });
-});
-
-$( "img" ).wrap( "<div class='imgcenter'></div>" );
